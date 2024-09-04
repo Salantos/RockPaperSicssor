9f9914d36c6cbd3a5dd175098006e85e31153835
Dennis Andersson <dennis.andersson@combitech.com>
2024-09-04 11:48:20 +0200

    Add play again

--- rockpapersicssor.py
+++ rockpapersicssor.py
@@ -1,25 +1,33 @@
 import random
 
-user_action = input("Enter a choice (Rock, Papper or Scissor): ")
-possible_actions = ["Rock", "Paper", "Sicssor"]
-computer_actions = random.choice(possible_actions)
+while True:
+    user_action = input("Enter a choice (Rock, Paper or Scissors): ").lower().capitalize()
+    possible_actions = ["Rock", "Paper", "Scissors"]
+    computer_actions = random.choice(possible_actions)
 
-print(f"\nYou chose {user_action}\nComputer chose {computer_actions}.\n")
+    print(f"\nYou chose {user_action}.\nComputer chose {computer_actions}.\n")
 
-if user_action == computer_actions:
-    print(f"Both players selected: {user_action}. It's a tie!")
-elif user_action == "Rock":
-    if computer_actions == "Sicssor":
-        print("Rock smashes Sicssor! You win")
-    else:
-        print("Paper covers Rock! You lose.")
-elif user_action == "Paper":
-    if computer_actions == "Rock":
-        print("Paper covers Rock! You win.")
-    else:
-        print("Sicssor cuts paper! You lose.")
-elif user_action == "Sicssor":
-    if computer_actions == "Paper":
-        print("Sicssor cuts paper! You win.")
-    else:
-        print("Rock smashes Sicssor! You lose.")
+    if user_action == computer_actions:
+        print(f"Both players selected: {user_action}. It's a tie!")
+    elif user_action == "Rock":
+        if computer_actions == "Scissors":
+            print("Rock smashes Scissors! You win")
+        else:
+            print("Paper covers Rock! You lose.")
+    elif user_action == "Paper":
+        if computer_actions == "Rock":
+            print("Paper covers Rock! You win.")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "Scissors":
+        if computer_actions == "Paper":
+            print("Scissors cuts paper! You win.")
+        else:
+            print("Rock smashes Scissors! You lose.")
+
+    play_again = input("Would you like to play again? (y/n): ").lower()
+    if play_again in ["yes", "y"]:
+        continue
+    elif play_again in ["no", "n"]:
+        print("Thanks for playing! Goodbye.")
+        break
