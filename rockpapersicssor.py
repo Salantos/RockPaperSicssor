e8b9fbf14c197d2575410a7c8bfbc40c935d2bc9
Dennis Andersson <dennis.andersson@combitech.com>
2024-09-04 11:21:18 +0200

    Add all checks

--- rockpapersicssor.py
+++ rockpapersicssor.py
@@ -8,3 +8,18 @@ print(f"\nYou chose {user_action}\nComputer chose {computer_actions}.\n")
 
 if user_action == computer_actions:
     print(f"Both players selected: {user_action}. It's a tie!")
+elif user_action == "Rock":
+    if computer_actions == "Sicssor":
+        print("Rock smashes Sicssor! You win")
+    else:
+        print("Paper covers Rock! You lose.")
+elif user_action == "Paper":
+    if computer_actions == "Rock":
+        print("Paper covers Rock! You win.")
+    else:
+        print("Sicssor cuts paper! You lose.")
+elif user_action == "Sicssor":
+    if computer_actions == "Paper":
+        print("Sicssor cuts paper! You win.")
+    else:
+        print("Rock smashes Sicssor! You lose.")
